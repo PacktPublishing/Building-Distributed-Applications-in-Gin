9fc7bbcb8ffecc157dd8da6842709fed8c867f7a
mlabouardy <mohamed@labouardy.com>
2021-05-24 22:18:44 +0200

    refactoring

--- chapter2/main.go
+++ chapter2/main.go
@@ -229,6 +229,5 @@ func main() {
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.GET("/recipes/:id", GetRecipeHandler)
-	router.GET("/recipes/search", SearchRecipesHandler)
 	router.Run()
 }
